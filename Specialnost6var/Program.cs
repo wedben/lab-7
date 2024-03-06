75a4705871459ffdf43deeb0066a3094e90720d2
Lavrikk <148620130+Lavrikk@users.noreply.github.com>
2024-03-07 00:35:45 +0300

    Add files via upload

---
+++ Specialnost6var/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Специальность
+    {
+        private string название;
+        private string сокращение;
+
+        public Специальность(string название, string сокращение)
+        {
+            this.название = название;
+            this.сокращение = сокращение;
+        }
+
+        public string Название
+        {
+            get { return название; }
+            set { название = value; }
+        }
+
+        public string Сокращение
+        {
+            get { return сокращение; }
+            set { сокращение = value; }
+        }
+    }
+}
