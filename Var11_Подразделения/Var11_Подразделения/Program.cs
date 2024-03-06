4f5c500c093fa3fd6eb71501f8a061b3c1da08b7
микирик <150595377+abyssalcrafted@users.noreply.github.com>
2024-03-06 18:46:40 +0300

    Add files via upload

---
+++ Var11_Подразделения/Var11_Подразделения/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+
+    public class Var11Подразделение
+    {
+        public string Название { get; } // ссылка на вариант с заданиями
+        public Var9Сотрудник Руководитель { get; } // ссылка на вариант с Руководителем
+        public Var12Организация Организация { get; } // ссылка на вариант с Организацией
+        public Var11Подразделение(string названиеПодразделения, Var9Сотрудник руководитель, Var12Организация организация)
+        {
+            Название = названиеПодразделения;
+            Организация = организация;
+            Руководитель = руководитель;
+        }
+    }
+}
