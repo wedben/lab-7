6111445c6eb5b892c209401010f9f906b4f2c6d9
Nikita Ukrasin <nikita.ykrasin@gmail.com>
2024-03-06 20:04:02 +0300

    Вар12

---
+++ Var12/Var12/Organizatsia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var12
+{
+    // вариант 12
+    internal class Организация
+    {
+        private string Название;
+        private string Юридический_адрес;
+        private string Фактический_адрес;
+        private string Руководитель;
+
+        public Организация(string Название, string Юридический_адрес, string Фактический_адрес, string Руководитель)
+        {
+            this.Название = Название;
+            this.Юридический_адрес = Юридический_адрес;
+            this.Фактический_адрес = Фактический_адрес;
+            this.Руководитель = Руководитель;
+        }
+    }
+}
---
+++ Var12/Var12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    internal class Организация
+    {
+        private string Название;
+        private string Юридический_адрес;
+        private string Фактический_адрес;
+        private string Руководитель;
+
+        public Организация(string Название, string Юридический_адрес, string Фактический_адрес, string Руководитель)
+        {
+            this.Название = Название;
+            this.Юридический_адрес = Юридический_адрес;
+            this.Фактический_адрес = Фактический_адрес;
+            this.Руководитель = Руководитель;
+        }
+    }
+}
