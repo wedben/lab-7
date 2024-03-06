81da36cd5d7d38c6a84d7e6ed2550615c2afeedb
ArtemKuznetsovv <151193669+ArtemKuznetsovv@users.noreply.github.com>
2024-03-06 19:57:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ТРПО;
+
+namespace ТРПО
+{
+    public class Var5Студент
+    {
+        public string Фамилия { get; }
+        public string Имя { get; }
+        public string Отчество { get; }
+        public DateTime ДатаРождения { get; }
+        public Var4Группа Группа { get; }
+        public Var5Студент(string фамилия, string имя, string отчество, Var4Группа группа, DateTime датаРождения)
+        {
+            Фамилия = фамилия;
+            Имя = имя;
+            Отчество = отчество;
+            ДатаРождения = датаРождения;
+            Группа = группа;
+        }
+    }
+}
+
