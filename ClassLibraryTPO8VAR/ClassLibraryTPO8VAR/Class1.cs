10b2db8c6ed91963b56b6ed136dd35653c7a388e
MatveyMM <148620193+MatveyMM@users.noreply.github.com>
2024-03-06 22:08:53 +0300

    Add files via upload

---
+++ ClassLibraryTPO8VAR/ClassLibraryTPO8VAR/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+
+namespace ClassLibaryTPO8VAR
+{
+    public class Смена
+    {
+        private string _название;
+
+        public Смена(string название, DateTime? дата = null)
+        {
+            _название = название;
+            if (дата == null)
+            {
+                дата = DateTime.Now;
+            }
+            // ваша логика по работе с датой
+        }
+    }
+}
+
---
+++ Var8MatthewTPO/Var8MatthewTPO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Var8MatthewTPO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+
+        public class Смена
+        {
+            private string Название;
+            private DateTime Дата;
+
+            public Смена(string название, DateTime? дата = null)
+            {
+                Название = название;
+                if (дата == null)
+                {
+                    Дата = DateTime.Now;
+                }
+                else
+                {
+                    Дата = (DateTime)дата;
+                }
+            }
+        }
+
+
+    }
+
+}
+
+
