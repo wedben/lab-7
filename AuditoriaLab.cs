8d875ac26898143cc4582f170b28aefb2d47227a
stepanzuzkov <151366745+stepanzuzkov@users.noreply.github.com>
2024-03-07 12:15:08 +0300

    Add files via upload

---
+++ AuditoriaLab.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Auditorium
+    {
+        private string name;
+        private string responsibleEmployee;
+        private int seatingCapacity;
+        private int windowCount;
+        private List<string> equipmentList;
+
+        public Auditorium(string name, string responsibleEmployee, int seatingCapacity, int windowCount, List<string> equipmentList)
+        {
+            this.name = name;
+            this.responsibleEmployee = responsibleEmployee;
+            this.seatingCapacity = seatingCapacity;
+            this.windowCount = windowCount;
+            this.equipmentList = equipmentList;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
