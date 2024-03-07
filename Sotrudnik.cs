d89cc21969a0f918a5ebfc4f03360cba9f47a84d
stroywer <96256239+stroywer@users.noreply.github.com>
2024-03-07 12:16:23 +0300

    Add files via upload

---
+++ Sotrudnik.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sotrudnik
+{
+
+    internal class Sotrudnik
+    {   
+        private string Familia;
+        private string Imia;
+        private string Otchestvo;
+        private string DoljnostName;
+
+        public Sotrudnik(string Familia, string Imia, string Otchestvo, string DoljnostName)
+        {
+            this.Familia = Familia;
+            this.Imia = Imia;
+            this.Otchestvo = Otchestvo;
+            this.DoljnostName = DoljnostName;
+        }
+
+    }
+}
