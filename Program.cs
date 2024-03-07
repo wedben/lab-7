20b388691418072d1d218e29d9d8588422220261
blvckizzz <151197927+blvckizzz@users.noreply.github.com>
2024-03-07 12:18:07 +0300

    Add files via upload
    Канищев Максим 3 варианта "Класс дисциплина"

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Var3Дисциплина
+    {
+        public string Название { get; }
+        public string Сокращение { get; }
+
+        public Var3Дисциплина(string название, string сокращение)
+        {
+            Название = название;
+            Сокращение = сокращение;
+        }
+    }
+}
+
