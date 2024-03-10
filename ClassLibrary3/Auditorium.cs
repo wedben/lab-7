bf74fee3d545411abe8f588e3dbe86c948fbdf2b
stepanzuzkov <151366745+stepanzuzkov@users.noreply.github.com>
2024-03-10 22:27:07 +0300

    Add files via upload

---
+++ ClassLibrary3/Auditorium.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Auditorium
+    {
+        private string name;
+        private string responsibleEmployee;
+        private int seatingCapacity;
+        private int windowCount;
+        private List<string> equipmentList;
+
+        public Auditorium(string name, string responsibleEmployee, int seatingCapacity, int windowCount, List<string> equipmentList)
+        {
+            this.name = name;
+            this.responsibleEmployee = responsibleEmployee;
+            this.seatingCapacity = seatingCapacity;
+            this.windowCount = windowCount;
+            this.equipmentList = equipmentList;
+        }
+    }
+}
