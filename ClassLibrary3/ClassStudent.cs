f676b5614ffa6c7589a3919631a62ca51ab5d68c
ArtemKuznetsovv <151193669+ArtemKuznetsovv@users.noreply.github.com>
2024-03-11 02:29:48 +0300

    Update ClassStudent.cs

--- ClassLibrary3/ClassStudent.cs
+++ ClassLibrary3/ClassStudent.cs
@@ -1,7 +1,25 @@
-﻿namespace ClassLibrary1
-{
-    public class Class1
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ClassLibrary1
+{
+    public class Var5Студент
+    {
+        public string Фамилия { get; }
+        public string Имя { get; }
+        public string Отчество { get; }
+        public DateTime ДатаРождения { get; }
+        public Var4Группа Группа { get; }
+        public Var5Студент(string фамилия, string имя, string отчество, Var4Группа группа, DateTime датаРождения)
+        {
+            Фамилия = фамилия;
+            Имя = имя;
+            Отчество = отчество;
+            ДатаРождения = датаРождения;
+            Группа = группа;
+        }
     }
 }
