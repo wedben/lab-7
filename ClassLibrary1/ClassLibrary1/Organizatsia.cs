639bbe14f512783eb4d6a7f49e8ed75e4614f936
Nikita Ukrasin <nikita.ykrasin@gmail.com>
2024-03-10 18:13:48 +0300

    класс организация

---
+++ ClassLibrary1/ClassLibrary1/Organizatsia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    // вариант 12
+    internal class Организация
+    {
+        private string Nazvanie;
+        private string Ur_adress;
+        private string Fakt_adress;
+        private string Rukovoditel;
+
+        public Организация(string Nazvanie, string Ur_adress, string Fakt_adress, string Rukovoditel)
+        {
+            this.Nazvanie = Nazvanie;
+            this.Ur_adress = Ur_adress;
+            this.Fakt_adress = Fakt_adress;
+            this.Rukovoditel = Rukovoditel;
+        }
+    }
+}
