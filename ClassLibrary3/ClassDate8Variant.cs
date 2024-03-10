607402380c95309ac9ec0e270e5e8575e079e054
MatveyMM <148620193+MatveyMM@users.noreply.github.com>
2024-03-10 23:10:59 +0300

    Add files via upload

---
+++ ClassLibrary3/ClassDate8Variant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+
+namespace ClassLibaryTPO8VAR
+{
+    public class Smena
+    {
+        private string _nazvanie;
+
+        public Smena(string nazvanie, DateTime? date = null)
+        {
+            _nazvanie = nazvanie;
+            if (date == null)
+            {
+                date = DateTime.Now;
+            }
+            
+        }
+    }
+}
+
