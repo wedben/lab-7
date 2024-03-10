6454eccdad761fd099c90dd21658d0454c244e41
Egor Lavrov <egorkalawr@mail.ru>
2024-03-10 22:20:02 +0300

    Создал класс Specialization

---
+++ ClassLibrary3/Specialization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Specialization
+{
+    public class Var6Specialization
+    {
+        private string Title { get; }
+        private string Reduction { get; }
+        public Var6Specialization(string Title, string Reduction)
+        {
+            this.Title = Title;
+            this.Reduction = Reduction;
+        }
+    }
+
+}
