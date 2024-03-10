a20423fcd4c35a1e5a007cbe88bf2a8eb07b13a9
Wak1ngj <151366972+Wak1ngj@users.noreply.github.com>
2024-03-10 20:04:59 +0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClassLibrary3;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
