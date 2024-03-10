029b1120467c225e6deae027d76ca86399c4e853
Кузнецов Артём <kuznetsov.aa2017@yandex.ru>
2024-03-11 02:26:31 +0300

    создал класс "Студент"

--- ClassLibrary3/obj/Debug/net7.0/ClassLibrary3.AssemblyInfo.cs
+++ ClassLibrary3/obj/Debug/net7.0/ClassLibrary3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+607402380c95309ac9ec0e270e5e8575e079e054")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
