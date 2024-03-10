38a165bd4630e6a40f2e2f3d2f691a201665571e
Maxim_Sachkov <maksimka0344@gmail.com>
2024-03-10 22:47:09 +0300

    Создал класс подразделения

---
+++ ClassLibrary3/Var11_Podrazdeleniya.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Var11Подразделение
+    {
+        public string Название { get; }
+        public Var9Сотрудник Руководитель { get; }
+        public Var12Организация Организация { get; }
+        public Var11Подразделение(string названиеПодразделения, Var9Сотрудник руководитель, Var12Организация организация)
+        {
+            Название = названиеПодразделения;
+            Организация = организация;
+            Руководитель = руководитель;
+        }
+    }
+}
