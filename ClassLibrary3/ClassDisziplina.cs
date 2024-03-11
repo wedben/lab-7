125e81c22fbfde4ca8cf77a7309297daee01aa7a
Кузнецов Артём <kuznetsov.aa2017@yandex.ru>
2024-03-11 12:01:51 +0300

    Создал класс Discipline

--- ClassLibrary3/ClassDisziplina.cs
+++ ClassLibrary3/ClassDisziplina.cs
@@ -1,7 +1,20 @@
-﻿namespace ClassLibrary2
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+namespace ClassLibrary1
 {
-    public class Class1
+    public class V3discipline
     {
-
+        public string Title { get; } 
+        public string Reduction { get;} 
+        public V3discipline(string title, string reduction)
+        {
+            Title = title;
+            Reduction = reduction;
+        }
     }
 }
+
