a2f275eed183ce6835be4a507978b85d9d29f5df
Nikitakrotov2005 <151366886+Nikitakrotov2005@users.noreply.github.com>
2024-03-21 22:19:42 +1300

    Add files via upload

---
+++ ClassLibrary3/Class1Группа.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+
+    public class Var4Группа
+    {
+        public string Название { get; }
+        public string Сокращение { get; }
+        public int Численность { get; }
+        public int ГодПоступления { get; }
+        public Var9Сотрудник КлассныйРуководитель { get; }
+        public Var6Специальность Специальность { get; }
+        public Var4Группа(string названиеГруппы, string сокращение, int численность, int годПоступления, Var9Сотрудник классныйРуководитель, Var6Специальность специальность)
+        {
+            Название = названиеГруппы;
+            Сокращение = сокращение;
+            Численность = численность;
+            ГодПоступления = годПоступления;
+            КлассныйРуководитель = классныйРуководитель;
+            Специальность = специальность;
+        }
+    }
+
+}
