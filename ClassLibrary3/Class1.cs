b3812f1910a6edad9ee5f5a3bcae0d63078afc65
ArtemKuznetsovv <151193669+ArtemKuznetsovv@users.noreply.github.com>
2024-03-21 12:31:24 +0300

    Update Class1.cs

--- ClassLibrary3/Class1.cs
+++ ClassLibrary3/Class1.cs
@@ -11,8 +11,6 @@
         private Var11 discipline;
         private Var09 employees;
         private Var02 theAudience;
-        private ClassStudent;
-        private ClassDisziplina;
         private Var04 group;
         private Var07 couple;
         private Var14 classes;
