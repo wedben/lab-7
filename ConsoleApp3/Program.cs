9c4b10fbfdc17362ddcd7ad469d1c308b4a83e5e
Wak1ngj <151366972+Wak1ngj@users.noreply.github.com>
2024-03-21 16:49:24 +0300

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,25 +9,6 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-                        while (true)
-            {
-                Console.Write("Введите имя создаваемого класса: ");
-                switch (Console.ReadLine())
-                {
-                    case "Аудитория": UI.Print(ClassCreator.Аудитория()); break;
-                    case "Дисциплина": UI.Print(ClassCreator.Дисциплина()); break;
-                    case "Группа": UI.Print(ClassCreator.Группа()); break;
-                    case "Пара": UI.Print(ClassCreator.Пара()); break;
-                    case "Должность": UI.Print(ClassCreator.Должность()); break;
-                    case "Студент": UI.Print(ClassCreator.Студент()); break;
-                    case "Организация": UI.Print(ClassCreator.Организация()); break;
-                    case "Смена": UI.Print(ClassCreator.Смена()); break;
-                    case "Подразделение": UI.Print(ClassCreator.Подразделение()); break;
-                    case "Сотрудник": UI.Print(ClassCreator.Сотрудник()); break;
-                    default: Console.WriteLine("Этот класс не реализован"); break;
-                }
-            }
-
         }
     }
 }
