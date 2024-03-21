38c35163a148609fc7aefa9b4fb208036510cb5d
Wak1ngj <151366972+Wak1ngj@users.noreply.github.com>
2024-03-21 16:35:33 +0300

    Update and rename Class1.cs to Var1Zanyatie.cs
    Поменял все полностью, надеюсь это не будет фатальной

---
+++ ClassLibrary3/Var1Zanyatie.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassLibrary3
+{
+    public class Var1Zanyatie
+    {
+        public DateTime dateTime { get; }
+        public V3discipline discipline { get; }
+        public employees sotrudnik { get; }
+        public Auditorium auditorium { get; }
+        public Var4Группа gruppa { get; }
+        //public Var7 { get; }
+        public var14 vidZ { get; }
+
+        public Var1Zanyatie(DateTime dateTime, V3discipline discipline, employees sotrudnik, Auditorium auditorium, Var4Группа gruppa, var14 vidZ)
+        {
+            dateTim = dateTim;
+            discipline = discipline;
+            sotrudnik = sotrudnik;
+            auditorium = auditorium;
+            gruppa = gruppa;
+            vidZ = vidZ;
+        }
+    }
+}
