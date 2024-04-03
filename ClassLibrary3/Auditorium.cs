7a234f567afa3124f74a13763aacc6d1c808cd37
stepanzuzkov <151366745+stepanzuzkov@users.noreply.github.com>
2024-04-03 19:09:12 +0300

    Auditorium.cs

--- ClassLibrary3/Auditorium.cs
+++ ClassLibrary3/Auditorium.cs
@@ -1,7 +1,20 @@
-﻿namespace ClassLibrary1
+namespace Auditorium
 {
-    public class Class1
+    public class Auditorium
     {
+        private string name;
+        private string responsibleEmployee;
+        private int seatingCapacity;
+        private int windowCount;
+        private List<string> equipmentList;
 
+        public Auditorium(string name, string responsibleEmployee, int seatingCapacity, int windowCount, List<string> equipmentList)
+        {
+            this.name = name;
+            this.responsibleEmployee = responsibleEmployee;
+            this.seatingCapacity = seatingCapacity;
+            this.windowCount = windowCount;
+            this.equipmentList = equipmentList;
+        }
     }
 }
