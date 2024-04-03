7223a9234105398f4f2aa4e46b4285164864881e
Wak1ngj <151366972+Wak1ngj@users.noreply.github.com>
2024-04-03 19:59:25 +0300

    Update Var1Zanyatie.cs

--- ClassLibrary3/Var1Zanyatie.cs
+++ ClassLibrary3/Var1Zanyatie.cs
@@ -8,21 +8,22 @@ namespace ClassLibrary3
     public class Var1Zanyatie
     {
         public DateTime dateTime { get; }
-        public V3discipline discipline { get; }
+        public V3discipline discipline2 { get; }
         public employees employees2 { get; }
-        public Auditorium2 auditorium { get; }
-        public Var4Группа gruppa { get; }
+        public Auditorium2 auditorium2 { get; }
+        public Var4Группа gruppa2 { get; }
         //public Var7 { get; }
-        public Vid vidZ { get; }
+        public Vid vidZ2 { get; }
 
         public Var1Zanyatie(DateTime dateTime, V3discipline discipline, employees sotrudnik, Auditorium2 auditorium, Var4Группа gruppa, Vid vidZ)
         {
             dateTime = dateTime;
-            discipline = discipline;
-            sotrudnik = sotrudnik;
-            auditorium = auditorium;
-            gruppa = gruppa;
-            vidZ = vidZ;
+            discipline2 = discipline;
+            employees2 = sotrudnik;
+            auditorium2 = auditorium;
+            gruppa2 = gruppa;
+            vidZ2 = vidZ;
         }
     }
 }
+
