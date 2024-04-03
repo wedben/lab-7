78bc0df1b1f5f6fbd4f96478fb63798e3bcee9b9
Wak1ngj <151366972+Wak1ngj@users.noreply.github.com>
2024-04-03 18:27:12 +0300

    Update ClassStudent.cs

--- ClassLibrary3/ClassStudent.cs
+++ ClassLibrary3/ClassStudent.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Var4Группа;
+using ClassLibrary1;
 
 namespace Student5var
 {
@@ -13,14 +13,15 @@ namespace Student5var
         public string Imya { get; }
         public string Otchestvo { get; }
         public DateTime Datarozdeniya { get; }
-        public Var4Группа { get; }
+
+        public Var4Группа группа { get; }
         public Var5(string Familia, string Imya, string Otchestvo, Var4Группа группа, DateTime Datarozdeniya)
         {
             Familia = Familia;
             Imya = Imya;
             Otchestvo = Otchestvo;
             Datarozdeniya = Datarozdeniya;
-            Var4Группа = Var4Группа;
+            группа = группа;
         }
     }
 }
