239961127f9320342f76285d2ae2ba45d826192d
Егор Яблоков <98410551+wedben@users.noreply.github.com>
2024-04-03 18:30:22 +0300

    Add files via upload

---
+++ ClassLibrary3/Vid_Zanyatia.cs
@@ -0,0 +1,29 @@
+﻿global using global::System;
+global using global::System.Collections.Generic;
+global using global::System.IO;
+global using global::System.Linq;
+global using global::System.Net.Http;
+global using global::System.Threading;
+global using global::System.Threading.Tasks;
+
+
+namespace Vid_Zanyatia
+{
+    public class Vid_Zanyatia
+    {
+        public class var14
+        {
+            private string name = "";
+
+            public var14(string name)
+            {
+                Name = name;
+            }
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+        }
+    }
+}
