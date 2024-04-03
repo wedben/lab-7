f046b2c1f7dc89c41ead630dd2b10e587f174d7d
stroywer <96256239+stroywer@users.noreply.github.com>
2024-04-03 18:56:54 +0300

    Update Var09_employees.cs

--- Var09_employees.cs
+++ Var09_employees.cs
@@ -4,46 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace employeessss 
+namespace GFD
 {
 
-    public class employees
-{
-    private string name = "";
-    private string surname = "";
-    private string patronymic = "";
-    private ТРПО_Лаба_2_вариант_10__должность_ Position;
+    internal class employees
+    {   
+        private string Familia { get; };
+        private string Imia { get; };
+        private string Otchestvo { get; };
+        private string position { get; };
 
-    public employees(string name, string surname, string patronymic, string post)
-    {
-        Name = name;
-        Surname = surname;
-        Patronymic = patronymic;
-        Post = Position;
+        public employees(string Familia, string Imia, string Otchestvo, string DoljnostName)
+        {
+            this.Familia = Familia;
+            this.Imia = Imia;
+            this.Otchestvo = Otchestvo;
+            this.DoljnostName = DoljnostName;
+        }
+
     }
-    public string Name
-    {
-        get { return name; }
-        set { name = value; }
-    }
-    public string Surname
-    {
-        get { return surname; }
-        set { surname = value; }
-    }
-    public string Patronymic
-    {
-        get { return patronymic; }
-        set { patronymic = value; }
-    }
-    public string Position
-    {
-        get { return post.ToString(); }
-        set { post = Var10_Post.ToString(value); }
-    }
-    internal static ТРПО_Лаба_2_вариант_10__должность_ ToString(string value)
-    {
-        throw new NotImplementedException();
-    }
-}
 }
